51df8cc5d25d0a322b1034e80647e15914d362f6
Patrik Olin <enmejl@gmail.com>
2018-12-03 19:19:10 +0100

    Har gjort ändringar som inte kom med från develop...

--- src/app/components/episode-details/episode-details.component.ts
+++ src/app/components/episode-details/episode-details.component.ts
@@ -22,9 +22,16 @@ export class EpisodeDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.loader.show();
-    const showId = this.route.params._value.showId;
-    const seasonId = this.route.params._value.seasonId;
-    const episodeId = this.route.params._value.episodeId;
+    let showId;
+    let seasonId;
+    let episodeId;
+    this.route.params.subscribe((res) => {
+      showId = res.showId;
+      seasonId = res.seasonId;
+      episodeId = res.episodeId;
+    });
+
+
     console.log(showId, seasonId, episodeId);
 
     this.apiCaller.getEpisodeDetails(showId, seasonId, episodeId)
--- src/app/shared/interfaces/show-details.ts
+++ src/app/shared/interfaces/show-details.ts
@@ -1,15 +1,19 @@
 import { Moment } from 'moment';
+import { Season } from './season';
 
 export interface ShowDetails {
   id: number;
   title: string;
   overview: string;
   first_air_date: string;
-  next_episode_to_air: string;
-  nextEpisodeLocal: Moment;
+  next_episode_to_air: {
+    air_date: string;
+  };
+  nextEpisodeLocal: Moment | string;
   status: string;
   poster_path: string;
   onWatchList: boolean;
   siteRating: number;
   airsTime: number;
+  seasons: Season[];
 }
